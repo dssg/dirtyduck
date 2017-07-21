90816e16251884aaba59f5828e8b50d6b4041fa8
Rayid Ghani <rayidghani@users.noreply.github.com>
2017-07-20 22:59:42 -0500

    Create create_inspections_table.sql

---
+++ ETL/create_inspections_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE inspections (
+	Inspection_ID VARCHAR(20)  NOT NULL,
+	DBA_Name VARCHAR(100),
+	AKA_Name VARCHAR(100),
+	License_Num DECIMAL,
+	Facility_Type VARCHAR(100),
+	Risk VARCHAR(15),
+	Address VARCHAR(100),
+	City VARCHAR(100),
+	State VARCHAR(2),
+	Zip VARCHAR(10),
+	Inspection_Date DATE,
+	Inspection_Type VARCHAR(100),
+	Results VARCHAR(100),
+	Violations VARCHAR(50000),
+	Latitude DECIMAL,
+	Longitude DECIMAL,
+	Location VARCHAR(40)
+);
