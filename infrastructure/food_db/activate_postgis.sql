fd6369edd1fb48fddb0b008636f90f1c956763b5
Adolfo De Unánue <nanounanue@gmail.com>
2017-08-06 18:30:28 -0500

    Cleaning infrastructure directory

---
+++ infrastructure/food_db/activate_postgis.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA postgis;
+
+ALTER DATABASE food SET search_path=public, postgis, contrib;
+
+CREATE EXTENSION postgis SCHEMA postgis;
+CREATE EXTENSION postgis_sfcgal SCHEMA postgis;
+CREATE EXTENSION pgrouting;
