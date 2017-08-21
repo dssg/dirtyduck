b5f08e4d0b05a107d8dd5d091b31215500f564c4
Adolfo De Unánue <nanounanue@gmail.com>
2017-08-21 16:48:58 -0500

    Removing from the cleaned tables all the rows which license_num is null.

--- src/create_violations_table.sql
+++ src/create_violations_table.sql
@@ -1,9 +1,9 @@
-drop table if exists cleaned.violations;
+drop table if exists cleaned.violations cascade;
 
 create table cleaned.violations as (
 select
 inspection,
-license_num,
+license_num as entity_id, -- This is a requirement of triage
 date as knowledge_date,
 btrim(tuple[1]) as violation_code,
 btrim(tuple[2]) as violation_description,
@@ -14,9 +14,9 @@ inspection,
 license_num,
 date,
 regexp_split_to_array(
-regexp_split_to_table(violations, '\|'),
+regexp_split_to_table(coalesce(violations, '.- Comments:'), '\|'),   -- We don't want to loose inspections
 '\.|- Comments:') as tuple
 from inspections
-where results in ('Fail', 'Pass', 'Pass w/ Conditions')
+where results in ('Fail', 'Pass', 'Pass w/ Conditions') and license_num is not null
 ) as t
 )
