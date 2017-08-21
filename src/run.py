4f6b254a2c784f1070c21b4ecd1234f4edfc6627
Adolfo De Unánue <nanounanue@gmail.com>
2017-08-20 23:54:50 -0500

    DB URL from environment

--- src/run.py
+++ src/run.py
@@ -1,15 +1,20 @@
+import os
 import sqlalchemy
 import yaml
 
 from catwalk.storage import FSModelStorageEngine
 from triage.experiments import SingleThreadedExperiment
 
+food_db = os.environ.get('FOOD_DB_URL')
+
+print(food_db)
+
 with open('inspections-training.yaml') as f:
     experiment_config = yaml.load(f)
 
 experiment = SingleThreadedExperiment(
     config=experiment_config,
-    db_engine=sqlalchemy.create_engine('postgresql://food_user:goli0808@food_db:5432/food'),
+    db_engine=sqlalchemy.create_engine(food_db),
     model_storage_class=FSModelStorageEngine,
     project_path='./triage-generated'
 )
