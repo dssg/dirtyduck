f40569e7ba9fc2f9305f1b4542dd267cbdf53fc0
Adolfo De Unánue <nanounanue@gmail.com>
2017-08-20 23:55:15 -0500

    Missing columns added

--- src/create_violations_table.sql
+++ src/create_violations_table.sql
@@ -1,13 +1,18 @@
 drop table if exists cleaned.violations;
 
 create table cleaned.violations as (
-select inspection,
+select
+inspection,
+license_num,
+date as knowledge_date,
 btrim(tuple[1]) as violation_code,
 btrim(tuple[2]) as violation_description,
 btrim(tuple[3]) as violation_comment from
 (
 select
 inspection,
+license_num,
+date,
 regexp_split_to_array(
 regexp_split_to_table(violations, '\|'),
 '\.|- Comments:') as tuple
