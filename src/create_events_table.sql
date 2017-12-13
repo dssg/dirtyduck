96fa09299d2335aab309672c85352581f3dcd75d
Adolfo De Unánue <nanounanue@gmail.com>
2017-12-13 16:47:31 -0600

    Split the README in eis and inspections. Events table created

---
+++ src/create_events_table.sql
@@ -0,0 +1,20 @@
+drop table if exists semantic.events cascade;
+
+create table semantic.events as (
+
+select i.inspection, type, i.license_num, facility_type, zip_code, city,
+       i.date, risk, results,
+json_agg(
+    json_build_object(
+        'code', v.code,
+	'description', v.description,
+	'comment', v.comment
+	)
+order  by code
+) as violations
+from cleaned.inspections as i
+inner join
+cleaned.violations as v
+on i.inspection = v.inspection
+group by i.inspection, type, i.license_num, facility_type, zip_code, city, i.date, risk, results
+)
