37eade6a3a464ba0023de8151f0838b256a4f028
Adolfo De Unánue <nanounanue@gmail.com>
2017-12-15 16:45:03 -0600

    Timechop plot

--- utils.py
+++ utils.py
@@ -29,16 +29,18 @@ chopper = timechop.Timechop(
     feature_end_time=np.max(df.date),
     label_start_time=np.min(df.date),
     label_end_time=np.max(df.date),
+
     model_update_frequency='1year',
-    training_label_timespans='6month',
-    test_label_timespans='6month',
-    max_training_histories='2year',
-    test_durations='3month',
+    training_label_timespans='3month',
     training_as_of_date_frequencies='3month',
+    max_training_histories='2year',
+
+    test_durations='3month',
+    test_label_timespans='3month',
     test_as_of_date_frequencies='1month'
 )
 
-def show_timechop(chopper):
+def show_timechop(chopper, show_as_of_times=True, show_boundaries=True):
 
     plt.close('all')
 
@@ -53,59 +55,61 @@ def show_timechop(chopper):
 
         max_training_history = chop['train_matrix']['max_training_history']
         test_label_timespan = chop['test_matrices'][0]['test_label_timespan']
+        training_label_timespan = chop['train_matrix']['training_label_timespan']
 
-        # Train matrix
-        ax[idx].hlines(
-          [x - 0.2 for x in range(len(train_as_of_times))],
-          [x.date() for x in train_as_of_times],
-          [x.date() - convert_str_to_relativedelta(max_training_history) for x in train_as_of_times],
-          linewidth=3, color=f"C{idx}",label=f"train_{idx}"
-        )
+        if(show_as_of_times):
+            # Train matrix (as_of_times)
+            ax[idx].hlines(
+              [x for x in range(len(train_as_of_times))],
+              [x.date() for x in train_as_of_times],
+              [x.date() + convert_str_to_relativedelta(training_label_timespan) for x in train_as_of_times],
+              linewidth=3, color=f"C{idx}",label=f"train_{idx}"
+            )
 
-        # Test matrix
-        ax[idx].hlines(
-          [x - 0.2 for x in range(len(test_as_of_times))],
-          [x.date() for x in test_as_of_times],
-          [x.date() + convert_str_to_relativedelta(test_label_timespan) for x in test_as_of_times],
-          linewidth=3, color=f"C{idx}",
-          label=f"test_{idx}"
-        )
+            # Test matrix
+            ax[idx].hlines(
+              [x for x in range(len(test_as_of_times))],
+              [x.date() for x in test_as_of_times],
+              [x.date() + convert_str_to_relativedelta(test_label_timespan) for x in test_as_of_times],
+              linewidth=3, color=f"C{idx}",
+              label=f"test_{idx}"
+            )
 
-        #ax.axvline(chop['feature_start_time'], color='k', linestyle='--')
-        #ax.axvline(chop['feature_end_time'], color='k', linestyle='--')
-        #ax.axvline(chop['label_start_time'] ,color='k', linestyle='--')
-        #ax.axvline(chop['label_end_time'] ,color='k', linestyle='--')
+            ax[idx].yaxis.set_major_locator(plt.NullLocator())
+            ax[idx].set_ylabel(f"Block {idx}")
 
-        # Limits: train
-        #ax.axvline(chop['train_matrix']['first_as_of_time'],color='k', linestyle='--')
-        #ax.axvline(chop['train_matrix']['last_as_of_time'],color='k', linestyle='--')
-        #ax.axvline(chop['train_matrix']['matrix_info_end_time'],color='k', linestyle='--')
+        if(show_boundaries):
+            # Limits: train
+            ax[idx].axvspan(chop['train_matrix']['first_as_of_time'],
+                            chop['train_matrix']['last_as_of_time'],
+                            color=f"C{idx}",
+                            alpha=0.3
+            )
 
 
-        # Limits: test
-        #ax.axvline(chop['test_matrices'][0]['first_as_of_time'] - convert_str_to_relativedelta(test_label_timespan) ,color='k', linestyle='--')
-        #ax.axvline(chop['test_matrices'][0]['last_as_of_time'],color='k', linestyle='--')
-        #ax.axvline(chop['test_matrices'][0]['matrix_info_end_time'],color='k', linestyle='--')
+            ax[idx].axvline(chop['train_matrix']['matrix_info_end_time'], color='k', linestyle='--')
 
-        ax[idx].xaxis.set_major_formatter(md.DateFormatter('%Y-%m'))
 
-        ax[idx].xaxis.set_major_locator(md.MonthLocator())
-        #ax.xaxis.set_minor_locator(md.WeekdayLocator(byweekday=md.MO))
-        #ax.xaxis.grid(b=True)
+            # Limits: test
+            ax[idx].axvspan(chop['test_matrices'][0]['first_as_of_time'],
+                            chop['test_matrices'][0]['last_as_of_time'],
+                            color=f"C{idx}",
+                            alpha=0.3
+            )
 
-        fig.autofmt_xdate()
+            ax[idx].axvline(chop['feature_start_time'], color='k', linestyle='--', alpha=0.2)
+            ax[idx].axvline(chop['feature_end_time'], color='k', linestyle='--',  alpha=0.2)
+            ax[idx].axvline(chop['label_start_time'] ,color='k', linestyle='--', alpha=0.2)
+            ax[idx].axvline(chop['label_end_time'] ,color='k', linestyle='--',  alpha=0.2)
 
-    # Limit: data
+            ax[idx].axvline(chop['test_matrices'][0]['matrix_info_end_time'],color='k', linestyle='--')
 
-    #ax.vlines([chop['feature_start_time'],chop['feature_end_time']], ymin=0, ymax=10)
+            ax[idx].xaxis.set_major_formatter(md.DateFormatter('%Y-%m'))
+            ax[idx].xaxis.set_major_locator(md.MonthLocator())
 
-    #ax.yaxis.set_ticklabels([x['label'] for x in data])
-    #ax.yaxis.set_ticks([idx for idx, x in enumerate(data)])
-    #ax.yaxis.grid(b=True)
-    #ax.set_ylim(len(data), -1)
+            fig.autofmt_xdate()
 
-    #ax.legend()
-
+    ax[0].set_title('Timechop blocks')
     fig.subplots_adjust(hspace=0)
     plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
     plt.show()
