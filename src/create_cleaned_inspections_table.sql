536e05340dcce4bebd7d700c5401152e9f3288d4
Adolfo De Unánue <nanounanue@gmail.com>
2017-12-23 00:22:07 -0600

    Minor changes

--- src/create_cleaned_inspections_table.sql
+++ src/create_cleaned_inspections_table.sql
@@ -14,7 +14,9 @@ btrim(lower(facility_type)) as facility_type,
 lower(substring(risk from '\((.+)\)')) as risk,
 btrim(lower(address)) as address,
 zip as zip_code,
-substring(btrim(lower(regexp_replace(type, 'liquor', 'task force', 'gi'))) from 'canvass|task force|complaint|food poisoning|consultation|license|tag removal') as type,
+substring(
+btrim(lower(regexp_replace(type, 'liquor', 'task force', 'gi')))
+from 'canvass|task force|complaint|food poisoning|consultation|license|tag removal') as type,
 date,
 ST_SetSRID(ST_MakePoint(longitude, latitude), 4326) as location
 from raw.inspections
