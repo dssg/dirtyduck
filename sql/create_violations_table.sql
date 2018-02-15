ba30e0c91df9f564da1f9527cd3758e52f5af0d2
Adolfo De Unánue <nanounanue@gmail.com>
2018-02-14 22:08:37 -0600

    Moved tangled from src to sql

---
+++ sql/create_violations_table.sql
@@ -0,0 +1,29 @@
+   drop table if exists cleaned.violations cascade;
+
+   create table cleaned.violations as (
+   select
+   inspection,
+   license_num, -- This is a requirement of triage
+   date,
+   btrim(tuple[1]) as code,
+   btrim(tuple[2]) as description,
+   btrim(tuple[3]) as comment,
+   (case
+     when btrim(tuple[1]) = '' then NULL
+     when btrim(tuple[1])::int between 1 and 14 then 'critical'
+     when btrim(tuple[1])::int between 15 and 29  then 'serious'
+     else 'minor'
+   end
+   ) as severity from
+   (
+   select
+   inspection,
+   license_num,
+   date,
+   regexp_split_to_array(
+   regexp_split_to_table(coalesce(violations, '.- Comments:'), '\|'),   -- We don't want to loose inspections
+   '\.|- Comments:') as tuple
+   from raw.inspections
+   where results in ('Fail', 'Pass', 'Pass w/ Conditions') and license_num is not null
+   ) as t
+   );
