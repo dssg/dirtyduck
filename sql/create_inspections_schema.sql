d787f6e51e278522472fe227b4cd13a7fe64c073
Adolfo De Unánue <nanounanue@gmail.com>
2018-02-18 23:33:16 -0600

    Cleaned a little bit. Added more to the user_metadata

---
+++ sql/create_inspections_schema.sql
@@ -0,0 +1,44 @@
+create schema if not exists inspections;
+
+create temp table inspections_outcomes as (
+select inspection, entity_id, date,
+   (result = 'fail') as failed,
+   (result = 'fail' and
+       ('serious' = ANY(array_agg(obj ->> 'severity')) or 'critical' = ANY(array_agg(obj ->> 'severity')))
+   ) as failed_major_violation
+from
+   (select inspection, entity_id, date, result, jsonb_array_elements(violations::jsonb) as obj from semantic.events)
+as t1
+group by inspection, entity_id, date, result
+);
+
+
+drop table if exists inspections.failed;
+
+create table inspections.failed as (
+select
+entity_id,
+date as outcome_date,
+failed as outcome
+from inspections_outcomes
+);
+
+
+drop table if exists inspections.failed_major_violation;
+
+create table inspections.failed_major_violation as (
+select
+entity_id,
+date as outcome_date,
+failed_major_violation as outcome
+from inspections_outcomes
+);
+
+drop table if exists inspections.active_facilities;
+
+create table inspections.active_facilities as (
+select
+distinct
+entity_id, 'active'::VARCHAR  as state, start_time, coalesce(end_time, '2020-12-31'::date) as end_time
+from semantic.entities
+);
