0a107bfcbaaed63d587821cd250f228487b97645
Adolfo De Unánue <nanounanue@gmail.com>
2019-02-12 12:15:17 -0600

    Fixed error with htmlize in publish.el. Changed name to whats_next in order to fit the regex in publish.el. Removed more files

--- org/publish.el
+++ org/publish.el
@@ -1,11 +1,30 @@
+(require 'package)
+
+(when (<= emacs-major-version 27)
+  (package-initialize) ;; Not needed in Emacs 27
+                                        ; Disable loading package again after init.el
+  )
+
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package htmlize
+  :defer t
+  )
+
 (require 'ox-publish)
 (setq org-publish-project-alist
       '(
 
         ("dirtyduck-notes"
-         :base-directory "~/projects/dsapp/dirtyduck/src/"
+         :base-directory "~/projects/dsapp/dirtyduck/org/"
          :base-extension "org"
-         :exclude "\d\d_.*\.org"
+         :exclude "[[:digit:]][[:digit:]]_.*\.org"
          :publishing-directory "~/projects/dsapp/dirtyduck/docs2/"
          :recursive t
          :publishing-function org-html-publish-to-html
@@ -15,7 +34,7 @@
          )
 
         ("dirtyduck-static"
-         :base-directory "~/projects/dsapp/dirtyduck/src/"
+         :base-directory "~/projects/dsapp/dirtyduck/org/"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|sql"
          :publishing-directory "~/projects/dsapp/dirtyduck/docs2/"
          :recursive t
