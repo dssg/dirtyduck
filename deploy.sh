62e566280ea9cf39a8f87fa35f7f5bac944e18a6
Adolfo De Unánue <nanounanue@gmail.com>
2019-02-20 11:05:36 -0600

    Bump to triagev3.3.0

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,16 @@
 #
 # //////////////////////////////////////////////////////////////////////////////
 
+source .aws_env
+
 # Exit the script as soon as something fails (-e) or if a variable is not defined (-u)
 set -e -u
 
 function info () {
 	echo "##############################################"
 	echo "#                                            #"
-	echo "#    Project: ${PROJECT_NAME} ver. ${PROJECT_VERSION}              #"
+	echo "#     Project: ${PROJECT_NAME}                    #"
+    echo "#     Triage ver. ${TRIAGE_VERSION}                      #"
 	echo "#                                            #"
 	echo "##############################################"
 	echo "Environment: ${ENV}"
@@ -32,7 +35,7 @@ function sync_to_s3 () {
     echo "#                                            #"
 	echo "##############################################"
 
-	aws s3 sync experiments/ s3://${S3_BUCKET}/experiments
+	aws s3 sync triage/experiment_config/ s3://${S3_BUCKET}/experiments
 }
 
 function sync_from_s3 () {
@@ -43,34 +46,27 @@ function sync_from_s3 () {
     echo "#                                            #"
 	echo "##############################################"
 
-	aws s3 sync s3://${S3_BUCKET}experiments/ experiments
+	aws s3 sync s3://${S3_BUCKET}experiments/ triage/experiment_config/
 }
 
 function update_jobs () {
-    tasks=triage
+	echo "Updating the job definition of the following tasks: ${PROJECT_NAME}"
 
-	echo "Updating the job definition of the following tasks: ${tasks}"
+	echo "+----------------------------------------+"
+	echo "|                                        |"
+	echo "| Updating  ${PROJECT_NAME} job definition"
+	echo "|                                        |"
+	echo "+----------------------------------------+"
 
-
-	for task in ${tasks}
-	do
-		echo "+----------------------------------------+"
-		echo "|                                        |"
-		echo "| Updating  ${task} job definition"
-		echo "|                                        |"
-		echo "+----------------------------------------+"
-
-	    aws batch register-job-definition --cli-input-json file://infrastructure/${task}-job-definition.json
-	done
+	aws batch register-job-definition --cli-input-json file://infrastructure/aws_batch/triage-job-definition.json
 }
 
-
-
 function update_images () {
+    echo "Updating images related to this project"
+
     tasks=triage
 
 	echo "Updating the image of the following tasks: ${tasks}"
-
 
 	for task in ${tasks}
 	do
@@ -79,33 +75,62 @@ function update_images () {
 		echo "| Updating ${task} image"
 		echo "|                                        |"
 		echo "+----------------------------------------+"
-		docker build --no-cache --tag dsapp/${PROJECT_NAME}/${task} infrastructure/
-		docker tag dsapp/${PROJECT_NAME}/${task} ${AWS_REGISTRY}/dsapp/${PROJECT_NAME}/${task}:${PROJECT_VERSION}
+		docker build --no-cache --tag dsapp/${PROJECT_NAME}/${task} infrastructure/${task}
+        docker tag dsapp/${PROJECT_NAME}/${task} ${AWS_REGISTRY}/dsapp/${PROJECT_NAME}/${task}:${TRIAGE_VERSION}
 		docker tag dsapp/${PROJECT_NAME}/${task} ${AWS_REGISTRY}/dsapp/${PROJECT_NAME}/${task}:latest
 
 		eval "$(aws ecr get-login --no-include-email --region us-west-2)"
 
-		docker push "${AWS_REGISTRY}"/dsapp/"${PROJECT_NAME}"/${task}:"${PROJECT_VERSION}"
+		docker push "${AWS_REGISTRY}"/dsapp/"${PROJECT_NAME}"/${task}:"${TRIAGE_VERSION}"
 		docker push "${AWS_REGISTRY}"/dsapp/"${PROJECT_NAME}"/${task}:latest
 	done
 
 }
 
+function update_triage_cli_image () {
+    tasks=triage-cli
+
+	echo "Updating the image of the following tasks: ${tasks}"
+
+	for task in ${tasks}
+	do
+		echo "+----------------------------------------+"
+		echo "|                                        |"
+		echo "| Updating ${task} image"
+		echo "|                                        |"
+		echo "+----------------------------------------+"
+#		docker build --no-cache --tag dsapp/${task} infrastructure/triage
+        docker tag dsapp/${task} ${AWS_REGISTRY}/dsapp/${task}:${TRIAGE_VERSION}
+		docker tag dsapp/${task} ${AWS_REGISTRY}/dsapp/${task}:latest
+
+		eval "$(aws ecr get-login --no-include-email --region us-west-2)"
+
+		docker push "${AWS_REGISTRY}"/dsapp/${task}:"${TRIAGE_VERSION}"
+		docker push "${AWS_REGISTRY}"/dsapp/${task}:latest
+	done
+
+}
+
+
 
 function run_experiment () {
-	environment_overrides=$1
+    job_name=$1
+    echo "Running job ${job_name}"
+
+	environment_overrides=$2
 	echo "Using environment_overrides: ${environment_overrides}"
 
-    parameters=$2
+    parameters=$3
     echo "Using parameters: ${parameters}"
 
-    command_overrides=${@:3}
+    command_overrides=${@:4}
 
     # # Retrieve temporary session credentials for current user
     session=$(aws sts get-session-token --duration-seconds 129600)  # 36 h
 
     # # Restructure these to mirror pipeline overrides
-    creds=$(<<<"$session" jq -f infrastructure/credentials.filter)
+    creds=$(<<<"$session" jq -f infrastructure/aws_batch/credentials.filter)
+
 
     # # Merge these AWS session credentials into *all* pipeline overrides
     overrides=$(
@@ -127,26 +152,17 @@ function run_experiment () {
 
 	fi
 
-    echo "Final overrides: ${overrides}"
-
-
     aws batch submit-job --job-queue ${AWS_JOB_QUEUE} \
-		--job-name ${PROJECT_NAME}-batch \
-        --job-definition ${PROJECT_NAME}-run-experiment \
+		--job-name ${job_name} \
+        --job-definition triage-cli-experiment \
         --container-overrides "${overrides}" \
         --parameters "${parameters}"
 }
 
 function run() {
-    run_experiment infrastructure/triage-overrides.json $@
+    run_experiment $1 infrastructure/aws_batch/triage-overrides.json "${@:2}"
 }
 
-function all () {
-	echo "Running everything!"
-	update-task-image
-	update-task-job
-    run $@
-}
 
 function help_menu () {
 cat << EOF
@@ -154,21 +170,21 @@ Usage: ${0} (-h | -i | -u | -b | -r | -a | --sync_{to,from}_s3 )
 OPTIONS:
    -h|--help                   Show this message
    -i|--info                   Show information about the environment
-   -b|--update-images          Build the ${PROJECT_NAME}'s triage image and push it to the AWS ECR
+   -t|--update-triage-image    Build the ${PROJECT_NAME}'s triage image and push it to the AWS ECR
    -u|--update-jobs            Update the ${PROJECT_NAME}'s triage job definition in AWS Batch
    -r|--run-experiment         Run experiments on ${PROJECT_NAME} data
-   -a|--all                    Creates images, pushes them the registry, updates the jobs and runs the pipeline
-   --sync-to-s3                Uploads the experiments and configuration files to s3://${S3_BUCKET}
-   --sync-from-s3              Gets the experiments and configuration files from s3://${S3_BUCKET}
+   --sync-to-s3                Uploads the experiments and configuration files to ${S3_BUCKET}
+   --sync-from-s3              Gets the experiments and configuration files from ${S3_BUCKET}
 EXAMPLES:
    Build and push the images to your AWS ECR:
         $ ./deploy.sh -b
    Update the job's definitions:
         $ ./deploy.sh -u
+   Sync your experiment config files:
+        $ ./deploy.sh --sync-to-s3
    Run triage experiments:
         $ ./deploy.sh -r --experiment_file=s3://${S3_BUCKET}/experiments/test.yaml,output_path=s3://${S3_BUCKET}/triage,replace=--replace
-   Everything!:
-        $ ./deploy.sh -a --experiment_file=s3://${S3_BUCKET}/experiments/test.yaml,output_path=s3://${S3_BUCKET}/triage,replace=--replace
+
 EOF
 }
 
@@ -181,6 +197,10 @@ fi
 case "${1}" in
   -b|--update-images)
   update_images
+  shift
+  ;;
+  -t|--update-triage-image)
+  update_triage_cli_image
   shift
   ;;
   -u|--update-jobs)
